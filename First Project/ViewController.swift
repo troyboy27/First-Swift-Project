3faf53971a5eda2c009e38dd32cb6e10002b16e6
Troy Abel <troyabel1@Troys-MacBook-Pro.local>
2016-05-14 12:36:22 -0500

    Button cycles through names

--- First Project/ViewController.swift
+++ First Project/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var names: [String] = ["Troy", "Ricky", "Donald", "Jesse", "Joan", "Daniel", "Alexa"]
+    var nameSpot : Int = 0;
 
+    @IBOutlet weak var announcementLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func changeNameClicked(sender: AnyObject) {
+        print("Hi, I clicked this button!")
+
+        nameSpot += 1
+        
+        print("nameSpot: \(nameSpot), nameSpot Remaindered: \(nameSpot % names.count), name: \(names[nameSpot % names.count])")
+        
+        announcementLabel.text = "\(names[nameSpot % names.count])'s First App"
+        
+        
+    }
 
 }
 
